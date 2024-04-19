45c3a969c741282e14cd67616321a6d9754b6ceb
Elchin <103633223+elchinguliev@users.noreply.github.com>
2024-04-19 09:24:59 +0400

    new

--- FunctionApp1225-v6/MyHttpTrigger1.cs
+++ FunctionApp1225-v6/MyHttpTrigger1.cs
@@ -8,11 +8,20 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using FunctionApp1225_v6.Models;
+using System.Collections.Generic;
 
 namespace FunctionApp1225_v6
 {
     public static class MyHttpTrigger1
     {
+
+
+        private static List<Player> players = new List<Player>
+        {
+            new Player { Id = 1, Name = "Elchin", Surname = "Guliyev" ,Score=100,TeamId=1},
+            new Player { Id = 2, Name = "Amin", Surname = "Atakishiyev" ,Score=90,TeamId=2},
+            new Player { Id = 3, Name = "Murad", Surname = "Azizov" ,Score=80,TeamId=3},
+        };
         [FunctionName("MyHttpTrigger1")]
         public static async Task<IActionResult> GetPlayerById(
             [HttpTrigger(AuthorizationLevel.Function, "get", Route = "Players/{id}")] HttpRequest req,
@@ -38,17 +47,6 @@ namespace FunctionApp1225_v6
            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = "Players")] HttpRequest req,
            ILogger log)
         {
-            //log.LogInformation("C# HTTP trigger function processed a request.");
-
-            //string name = req.Query["name"];
-
-            //string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            //dynamic data = JsonConvert.DeserializeObject(requestBody);
-            //name = name ?? data?.name;
-
-            //string responseMessage = string.IsNullOrEmpty(name)
-            //    ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-            //    : $"Hello, {name}. This HTTP triggered function executed successfully.";
             string id = req.Query["id"];
             return new OkObjectResult(id);
         }
@@ -62,6 +60,15 @@ namespace FunctionApp1225_v6
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             Player player = JsonConvert.DeserializeObject<Player>(requestBody);
             return new OkObjectResult(player);
+        }
+
+        [FunctionName("MyHttpTrigger4")]
+        public static async Task<IActionResult> DeletePlayerById(
+        [HttpTrigger(AuthorizationLevel.Function, "delete", Route = "Players/Delete/{id}")] HttpRequest req,
+        ILogger log, int id)
+        {
+            // Add your logic to delete the player with the given ID
+            return new OkObjectResult($"Player with ID {id} has been deleted.");
         }
     }
 }
